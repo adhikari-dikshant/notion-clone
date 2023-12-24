a2a7512cc69cbb7fb13f0a05622e51ff327080aa
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2023-12-24 11:25:08 +0530

    dashboard setup

--- src/components/landing-page/title-section.tsx
+++ src/components/landing-page/title-section.tsx
@@ -12,10 +12,9 @@ const TitleSection: React.FC<TitleSectionProps> = ({
 }) => {
     return (
         <React.Fragment>
-            <section
-                className="flex flex-col gap-4 justify-center items-start md:items-center">
+            <section className="flex flex-col gap-4 justify-center items-start md:items-center">
                 <article className="rounded-full p-[1px] text-sm dark:bg-gradient-to-r dark:from-brand-primaryBlue dark:to-brand-primaryPurple">
-                    <div className="rounded-full px-3py-1 dark:bg-black">
+                    <div className="rounded-full px-3 py-1 dark:bg-black">
                         {pill}
                     </div>
                 </article>
